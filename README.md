d135cb0889cf991dbddcc4b00f8d995646865de8
Captainpast <captainpast@gmx.de>
2023-09-02 11:39:29 +0200

    add new parameter to uninstall

--- README.md
+++ README.md
@@ -1,13 +1,22 @@
 # Edge-Fixer
 
-_a programme the replaces the the Microsoft Edge and Bing in the start menu search_
-_currently with Firefox and DuckDuckGo (In future it will be configurable)_
+_A program that replaces Microsoft Edge and Bing in the start menu search._ \
+_Currently, with Firefox and DuckDuckGo (in the future, it will be configurable)._
 
 # Install
 
-1. Download the newest exe from the [release section](https://gitlab.com/Captainpast/edge-fixer/-/releases)
-2. Move it to a folder of you choice (do not move the exe after execution)
-3. Run the exe as Administrator (via right click), there will no response
+1. Download the newest EXE from the [release section](https://gitlab.com/Captainpast/edge-fixer/-/releases).
+2. Move it to a folder of your choice (do not move the EXE after execution).
+3. Run the EXE as an administrator (via right-click). There will be no response.
+
+# Uninstall
+
+1. Navigate to your chosen folder for the EXE.
+2. Open the advanced context menu via `shift + right-click` and click _Open CMD/Powershell here_.
+3. Type `./edge-fixer.exe uninstall` and enter. There should be positive feedback.
 
 # Build
 WIP
+```
+cargo build --release
+```
--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,19 @@ use regex::Regex;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let mut uninstall_arg = false;
     let mut is_debugger = false;
     for arg in args {
-        if arg == "--as-debugger" {
+        if arg == "uninstall" {
+            uninstall_arg = true;
+        } else if arg == "--as-debugger" {
             is_debugger = true;
         }
     }
-    if is_debugger {
+
+    if uninstall_arg {
+        uninstall()
+    } else if is_debugger {
         decode_url()
     } else {
         install()
@@ -54,13 +60,14 @@ fn open_browser(url: String) {
         .unwrap();
 }
 
+
+const ETC_HOSTS: &str = "C:/Windows/System32/drivers/etc/hosts";
+const HOST_REDIRECT: &str = "0.0.0.0 www.bing.com # by edge fixer";
+
 fn install() {
     println!("Hello, to Edge Fixer!\n");
 
     print!("edit host file...");
-
-    const ETC_HOSTS: &str = "C:/Windows/System32/drivers/etc/hosts";
-    const HOST_REDIRECT: &str = "0.0.0.0 www.bing.com # by edge fixer";
 
     let mut file_text = fs::read_to_string(ETC_HOSTS).unwrap();
     file_text = file_text.replace("\r", "");
@@ -97,5 +104,36 @@ fn install() {
     let current_exe = env::current_exe().unwrap();
     key.set_value("Debugger", &format!("{} --as-debugger", current_exe.display())).unwrap();
 
+    println!("success\n");
+}
+
+fn uninstall() {
+    println!("Removing Edge Fixer\n");
+
+    print!("edit host file...");
+
+    let mut file_text = fs::read_to_string(ETC_HOSTS).unwrap();
+    file_text = file_text.replace("\r", "");
+
+    let splitted_text = file_text.split("\n");
+    let mut new_text = String::from("");
+
+    for line in splitted_text {
+        if !line.contains("# by edge fixer") {
+            new_text = format!("{}{}\n", new_text, line);
+        }
+    }
+
+    fs::write(ETC_HOSTS, new_text).unwrap();
+    
+    println!("success\n");
+
+
+    print!("edit regestry...");
+
+    let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
+    let cur_ver = hklm.open_subkey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Image File Execution Options").unwrap();
+    cur_ver.delete_subkey_all("msedge.exe").unwrap();
+    
     println!("success\n");
 }
